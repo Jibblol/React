45ac15d93ed1e5ed5703e8e4c45b3ad5ce817d6c
Vegard <Jibblol@gmail.com>
2018-10-12 10:27:46 +0200

    Initial appBar with faults

--- whatsthatgame/src/components/DynamicTabView.js
+++ whatsthatgame/src/components/DynamicTabView.js
@@ -1,69 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import GameList from './GameList.js';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import GameList from "./GameList.js";
 
 function TabContainer(props) {
-    return (
-        <Typography component="div" style={{ padding: 8 * 3 }}>
-            {props.children}
-        </Typography>
-    );
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
 }
 
 TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
-        backgroundColor: (64, 70, 70)
-    },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    backgroundColor: (64, 70, 70)
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
 });
 
 class DynamicTabView extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            value: 0,
-        };
-    }
-
-    handleChange = (event, value) => {
-        this.setState({ value });
+    this.state = {
+      value: 0
     };
+  }
 
-    render() {
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
 
-        const { classes } = this.props;
-        const { value } = this.state;
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
 
-        return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <Tabs value={value} onChange={this.handleChange} centered>
-                        <Tab label="Item One" />
-                        <Tab label="Item Two" />
-                        <Tab label="Item Three" />
-                    </Tabs>
-                </AppBar>
-                {value === 0 && <TabContainer><GameList /></TabContainer>}
-                {value === 1 && <TabContainer>Item Two</TabContainer>}
-                {value === 2 && <TabContainer>Item Three</TabContainer>}
-            </div>
-        )
-    }
-
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+            </IconButton>
+            {/* <Typography variant="title" color="inherit" className={classes.grow}>
+                News
+            </Typography> */}
+            <Tabs value={value} onChange={this.handleChange} centered>
+              <Tab label="Item One" />
+              <Tab label="Item Two" />
+              <Tab label="Item Three" />
+            </Tabs>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        {value === 0 && (
+          <TabContainer>
+            <GameList />
+          </TabContainer>
+        )}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+      </div>
+    );
+  }
 }
 DynamicTabView.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(DynamicTabView);
